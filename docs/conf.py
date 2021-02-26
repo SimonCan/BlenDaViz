361b19e0b6bd19537af529df0f83fc53acb1af86
SimonCan <simon.candelaresi@gmail.com>
2021-02-26 16:38:50 +0000

    Reverted change in index dir.

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'docs/index'
+master_doc = 'index'
 
 # General information about the project.
 project = u'BlenDaViz'
