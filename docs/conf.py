dcfe40f0117e8cc434c918d67be13be70791d7a3
SimonCan <simon.candelaresi@gmail.com>
2021-02-26 16:08:32 +0000

    Removed autoapi on sphinx doc conf.

--- docs/conf.py
+++ docs/conf.py
@@ -35,8 +35,7 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'autoapi.extension']
+    'sphinx.ext.githubpages',]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
