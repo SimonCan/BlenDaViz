0d5378ea7aefb41d56fc7782ce9b3dacecbff618
SimonCan <simon.candelaresi@gmail.com>
2024-07-11 13:58:23 +0100

    Corrected array size checks.

--- __init__.py
+++ __init__.py
@@ -9,9 +9,9 @@ import bpy
 from .generic import GenericPlot
 from .plot1d import plot, PathLine
 from .plot2d import mesh, Surface
-from .plot3d import vol, Volume, quiver, Quiver3d, contour, Contour3d
+from .plot3d import quiver, Quiver3d, contour, Contour3d
 from .streamlines3d import streamlines_function, streamlines_array, \
-    Streamline3d, Streamline3dArray
+      Streamline3d, Streamline3dArray
 from .matplotlib_bridge import mpl_figure_to_blender, MPLEmbedding
 from .colors import string_to_rgba, make_rgba_array
 from .vectors import vec, arrow
--- plot3d.py
+++ plot3d.py
@@ -7,153 +7,153 @@ Contains routines to three-dimensional plots.
 from blendaviz.generic import GenericPlot
 
 
-def vol(phi, x, y, z, emission=None, color_map=None):
-    """
-    Plot a 3d volume rendering of a scalar field.
+#def vol(phi, x, y, z, emission=None, color_map=None):
+    #"""
+    #Plot a 3d volume rendering of a scalar field.
 
-    Signature:
+    #Signature:
 
-    vol(phi, x, y, z, alpha=None, color_map=None)
+    #vol(phi, x, y, z, alpha=None, color_map=None)
 
-    Parameters
-    ----------
-    phi:  Scalar field in 3d of shape (nx, ny, nz).
+    #Parameters
+    #----------
+    #phi:  Scalar field in 3d of shape (nx, ny, nz).
 
-    x, y, z:  1d arrays for the coordinates.
+    #x, y, z:  1d arrays for the coordinates.
 
-    emission:  1d or 2d array of floats containing the emission values for phi.
-        If specified as 2d array fo shape (2, n_emission) the emission[0, :] are the values
-        phi which must be monotonously increasing and emission[1, ;] are their corresponding
-        emission values.
+    #emission:  1d or 2d array of floats containing the emission values for phi.
+        #If specified as 2d array fo shape (2, n_emission) the emission[0, :] are the values
+        #phi which must be monotonously increasing and emission[1, ;] are their corresponding
+        #emission values.
 
-    color_map:  Color map for the values stored in the array 'c'.
-        These are the same as in matplotlib.
+    #color_map:  Color map for the values stored in the array 'c'.
+        #These are the same as in matplotlib.
 
-    Returns
-    -------
-    Volume plot object.
-    """
+    #Returns
+    #-------
+    #Volume plot object.
+    #"""
 
-    import inspect
+    #import inspect
 
-    # Assign parameters to the Volume objects.
-    volume_return = Volume()
-    argument_dict = inspect.getargvalues(inspect.currentframe()).locals
-    for argument in argument_dict:
-        setattr(volume_return, argument, argument_dict[argument])
-    volume_return.plot()
-    return volume_return
-
-
-class Volume(GenericPlot):
-    """
-    Volume class including the data, 3d texture and parameters.
-    """
-
-    def __init__(self):
-        """
-        Fill members with default values.
-        """
-
-        self.phi = 0
-        self.x = 0
-        self.y = 0
-        self.z = 0
-        self.emission = None
-        self.color_map = None
-        self.mesh_object = None
-        self.mesh_material = None
+    ## Assign parameters to the Volume objects.
+    #volume_return = Volume()
+    #argument_dict = inspect.getargvalues(inspect.currentframe()).locals
+    #for argument in argument_dict:
+        #setattr(volume_return, argument, argument_dict[argument])
+    #volume_return.plot()
+    #return volume_return
 
 
-    def plot(self):
-        """
-        Plot the 3d texture.
-        """
+#class Volume(GenericPlot):
+    #"""
+    #Volume class including the data, 3d texture and parameters.
+    #"""
 
-#         import bpy
-#         import numpy as np
-#         import matplotlib.cm as cm
+    #def __init__(self):
+        #"""
+        #Fill members with default values.
+        #"""
 
-#         # Check the validity of the input arrays.
-#         if (not isinstance(self.x, np.ndarray) or not isinstance(self.y, np.ndarray)
-#                 or not isinstance(self.z, np.ndarray)):
-#             print("Error: x OR y OR z array invalid.")
-#             return -1
-#         if not isinstance(self.phi, np.ndarray):
-#             print("Error: phi must be numpy array.")
-#             return -1
-#         if self.emission:
-#             if not isinstance(self.emission, np.ndarray):
-#                 print("Error: emission must be numpy array.")
-#                 return -1
-
-#         # Using volumetric textures or voxels?
-
-#         # Delete existing meshes.
-#         if not self.mesh_object is None:
-#             bpy.ops.object.select_all(action='DESELECT')
-#             self.mesh_object.select = True
-#             bpy.ops.object.delete()
-#             self.mesh_object = None
-
-#         # Delete existing materials.
-#         if not self.mesh_material is None:
-#             bpy.data.materials.remove(self.mesh_material)
-
-#         # Create cuboid.
-#         bpy.ops.mesh.primitive_cube_add()
-#         self.mesh_object = bpy.context.object
-
-#         # Define the RGB value for each voxel.
-#         phi_max = np.max(self.phi)
-#         phi_min = np.min(self.phi)
-#         if self.color_map is None:
-#             self.color_map = cm.viridis
-# #        pixels[0::3] = self.color_map((self.phi.flatten() - phi_min)/(phi_max - phi_min))[:, 0]
-# #        pixels[1::3] = self.color_map((self.phi.flatten() - phi_min)/(phi_max - phi_min))[:, 1]
-# #        pixels[2::3] = self.color_map((self.phi.flatten() - phi_min)/(phi_max - phi_min))[:, 2]
-
-#         # Define the emission for each voxel.
-#         emission = np.zeros_like(self.phi)
-#         if self.emission.ndim == 1:
-#             phi_emission = np.zeros([2, self.emission.size])
-#             phi_emission[0, :] = np.linspace(self.phi.min(), self.size.max(), self.emission.size)
-#             phi_emission[1, :] = self.emission
-#         else:
-#             phi_emission = self.emission
-
-#         for emission_idx in self.emission.shape[-1]-1:
-#             mask = self.phi >= phi_emission[0, emission_idx] and self.phi < phi_emission[0, emission_idx+1]
-#             weight_left = self.phi[mask] - phi_emission[0, emission_idx]
-#             weight_right = -self.phi[mask] + phi_emission[0, emission_idx+1]
-#             emission[mask] = (weight_left*phi_emission[1, emission_idx] + \
-#                               weight_right*phi_emission[1, emission_idx+1]) \
-#                              /(weight_left + weight_right)
-#         del(emission)
-
-#         # Assign a material to the cuboid.
-#         self.mesh_material = bpy.data.materials.new('MaterialMesh')
-#         self.mesh_material.use_nodes = True
-
-#         # Add the RGB and emission values to the material.
-#         nodes = node_tree.nodes
-#         # Remove diffusive BSDF node.
-#         nodes.remove(nodes[1])
-#         # Add the emission shader.
-#         node_emission = nodes.new(type='ShaderNodeEmission')
-#         # Link the emission output to the material output.
-#         node_tree.links.new(node_emission.outputs['Emission'],
-#                             nodes[0].inputs['Volume'])
-#         # Add the RGB source node.
-
-#         # Link the RGB output to the emission shader  color input.
-
-#         # Add the emission source node.
-
-#         # Link the emission output to the emission shader strength input.
+        #self.phi = 0
+        #self.x = 0
+        #self.y = 0
+        #self.z = 0
+        #self.emission = None
+        #self.color_map = None
+        #self.mesh_object = None
+        #self.mesh_material = None
 
 
-        return 0
+    #def plot(self):
+        #"""
+        #Plot the 3d texture.
+        #"""
+
+        #import bpy
+        #import numpy as np
+        #import matplotlib.cm as cm
+
+        ## Check the validity of the input arrays.
+        #if (not isinstance(self.x, np.ndarray) or not isinstance(self.y, np.ndarray)
+                #or not isinstance(self.z, np.ndarray)):
+            #print("Error: x OR y OR z array invalid.")
+            #return -1
+        #if not isinstance(self.phi, np.ndarray):
+            #print("Error: phi must be numpy array.")
+            #return -1
+        #if self.emission:
+            #if not isinstance(self.emission, np.ndarray):
+                #print("Error: emission must be numpy array.")
+                #return -1
+
+        ## Using volumetric textures or voxels?
+
+        ## Delete existing meshes.
+        #if not self.mesh_object is None:
+            #bpy.ops.object.select_all(action='DESELECT')
+            #self.mesh_object.select = True
+            #bpy.ops.object.delete()
+            #self.mesh_object = None
+
+        ## Delete existing materials.
+        #if not self.mesh_material is None:
+            #bpy.data.materials.remove(self.mesh_material)
+
+        ## Create cuboid.
+        #bpy.ops.mesh.primitive_cube_add()
+        #self.mesh_object = bpy.context.object
+
+        ## Define the RGB value for each voxel.
+        #phi_max = np.max(self.phi)
+        #phi_min = np.min(self.phi)
+        #if self.color_map is None:
+            #self.color_map = cm.viridis
+##        pixels[0::3] = self.color_map((self.phi.flatten() - phi_min)/(phi_max - phi_min))[:, 0]
+##        pixels[1::3] = self.color_map((self.phi.flatten() - phi_min)/(phi_max - phi_min))[:, 1]
+##        pixels[2::3] = self.color_map((self.phi.flatten() - phi_min)/(phi_max - phi_min))[:, 2]
+
+        ## Define the emission for each voxel.
+        #emission = np.zeros_like(self.phi)
+        #if self.emission.ndim == 1:
+            #phi_emission = np.zeros([2, self.emission.size])
+            #phi_emission[0, :] = np.linspace(self.phi.min(), self.size.max(), self.emission.size)
+            #phi_emission[1, :] = self.emission
+        #else:
+            #phi_emission = self.emission
+
+        #for emission_idx in self.emission.shape[-1]-1:
+            #mask = self.phi >= phi_emission[0, emission_idx] and self.phi < phi_emission[0, emission_idx+1]
+            #weight_left = self.phi[mask] - phi_emission[0, emission_idx]
+            #weight_right = -self.phi[mask] + phi_emission[0, emission_idx+1]
+            #emission[mask] = (weight_left*phi_emission[1, emission_idx] + \
+                              #weight_right*phi_emission[1, emission_idx+1]) \
+                             #/(weight_left + weight_right)
+        #del(emission)
+
+        ## Assign a material to the cuboid.
+        #self.mesh_material = bpy.data.materials.new('MaterialMesh')
+        #self.mesh_material.use_nodes = True
+
+        ## Add the RGB and emission values to the material.
+        #nodes = node_tree.nodes
+        ## Remove diffusive BSDF node.
+        #nodes.remove(nodes[1])
+        ## Add the emission shader.
+        #node_emission = nodes.new(type='ShaderNodeEmission')
+        ## Link the emission output to the material output.
+        #node_tree.links.new(node_emission.outputs['Emission'],
+                            #nodes[0].inputs['Volume'])
+        ## Add the RGB source node.
+
+        ## Link the RGB output to the emission shader  color input.
+
+        ## Add the emission source node.
+
+        ## Link the emission output to the emission shader strength input.
+
+
+        #return 0
 
 
 
@@ -335,7 +335,7 @@ class Quiver3d(GenericPlot):
         arrays_with_time_list = ['x', 'y', 'z', 'u', 'v', 'w']
         for array_with_time in arrays_with_time_list:
             array_value = getattr(self, array_with_time)
-            if array_value.ndim == 3:
+            if (array_value.ndim == 1) or (array_value.ndim == 3):
                 setattr(self, '_' + array_with_time, array_value.ravel())
             else:
                 setattr(self, '_' + array_with_time, array_value[..., self.time_index].ravel())
@@ -362,6 +362,10 @@ class Quiver3d(GenericPlot):
         if isinstance(self.color, str):
             if self.color == 'magnitude':
                 self.color = np.sqrt(self._u**2 + self._v**2 + self._w**2)
+        if self.vmin == self.vmax:
+            self.vmax = 2*self.color.min()
+        if (self.vmin == 0) and (self.vmax == 0):
+            self.vmax = 1.0
         color_rgba = colors.make_rgba_array(self.color, self._x.shape[0],
                                             self.color_map, self.vmin, self.vmax)
 
