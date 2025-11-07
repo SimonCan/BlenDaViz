0fb2443d7862c79226d58ac797d0a3a9eedcf1a3
SimonCan <simon.candelaresi@gmail.com>
2025-11-07 10:27:05 +0000

    Added very basic testing routine. This will be expanded to better covering the full library.

---
+++ blendaviz/tests/test_all.py
@@ -0,0 +1,36 @@
+"""
+Run all BlenDaViz tests inside Blender:
+"""
+
+import unittest
+import numpy as np
+import blendaviz as blt
+import bpy
+import sys
+
+
+class Plot1d(unittest.TestCase):
+    def test_plot1d(self):
+        y = np.linspace(0, 6*np.pi, 20)
+        x = 2*np.cos(y/2)
+        z = 2*np.sin(y/2)
+
+        pl = blt.plot(x, y, z, marker='cube', radius=0.7)
+
+        self.assertIsNotNone(pl, "blt.plot() returned None")
+
+        objects = list(bpy.data.objects)
+        self.assertGreater(len(objects), 0, "No Blender objects were created by plot().")
+
+
+def run_tests():
+    suite = unittest.defaultTestLoader.loadTestsFromTestCase(Plot1d)  # ✅ fixed class name
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    sys.stdout.flush()
+    sys.stderr.flush()
+    bpy.ops.wm.quit_blender()
+
+    return result
+
+
+run_tests()
